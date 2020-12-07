dc2be191443449c091665dbed0856e09bd98acf4
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-08 01:35:36 +0600

    Local Variable

---
+++ local.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	function myname(){
+		$name="arif";
+		echo $name;
+	}
+	myname();
+	echo $name;
+
+	// Note: Local vaeiable Function ar bitor a value nite hobe. function ar bitor a echo korte hobe
+
+	?>
+</body>
+</html>
