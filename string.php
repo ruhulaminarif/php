f8f6847efb70e99391cb211dedf7da998a04bbeb
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-09 02:26:59 +0600

    strlen
    strlen, str_word_count , strrev, strpos , str_replace

---
+++ string.php
@@ -0,0 +1,14 @@
+<?php
+$x="Hello Bangladesh";
+
+echo strlen($x);
+echo "<br>";
+echo str_word_count($x);
+echo "<br>";
+echo strrev($x);
+echo "<br>";
+echo strpos($x,"esh");
+echo "<br>";
+echo str_replace("Bangladesh","Noakhali",$x);
+
+?>
