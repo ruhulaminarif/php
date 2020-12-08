61084b3c41bdccb6ad4fe958f98a4a5270557de4
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-08 23:39:17 +0600

    Variable

---
+++ variable1.php
@@ -0,0 +1,21 @@
+<?php
+$font="verdana";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<style>
+		body{
+			font-family: <?php echo $font; ?>;
+			
+		}
+	</style>
+</head>
+<body>
+	<p>Lorem ipsum, dolor, sit amet consectetur adipisicing elit. Doloribus pariatur quam, voluptatibus, quae dicta eaque debitis unde. Laboriosam culpa excepturi nobis amet rem necessitatibus iure dicta, magni nostrum, omnis, error.</p>
+</body>
+</html>
