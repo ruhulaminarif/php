9ff1756bf1a1451129cf6336a47fe4c3053c9f40
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-09 01:30:49 +0600

    PHP Data Type

---
+++ data_type.php
@@ -0,0 +1,64 @@
+
+
+<!-- $x = "Hello World" ; String
+$x = 25 ; Integer
+$x = 30.5 ; Float
+$x = True/false ; Boolean
+$x = array("HTML","CSS","JS") ; Array(multiple value)
+$x = new MtClass() ; Object
+$x = null ; Null -->
+
+<?php
+$x="Hello Bangladesh";
+
+echo $x . "<br>";
+var_dump($x);
+echo "<br>";
+?>
+
+<?php
+$x=25;
+echo $x . "<br>";
+var_dump($x);
+echo "<br>";
+?>
+
+<?php
+$x=25.5;
+echo $x . "<br>";
+var_dump($x);
+echo "<br>";
+?>
+
+<?php
+$x=true;
+echo $x . "<br>";
+var_dump($x);
+echo "<br>";
+?>
+
+<?php
+$x=array("HTML","CSS","JS");
+echo $x[2] . "<br>";
+var_dump($x);
+echo "<br>";
+?>
+
+<?php
+$x=array("HTML","CSS","JS");
+echo $x[2] . "<br>";
+var_dump($x);
+echo "<br>";
+?>
+<?php
+$x=null;
+echo $x . "<br>";
+var_dump($x);
+echo "<br>";
+?>
+
+
+
+
+
+
