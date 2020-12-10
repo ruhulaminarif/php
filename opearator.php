542f83d3b19773823bc68647027cecf2454768e9
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-11 01:13:12 +0600

    PHP Operator

---
+++ opearator.php
@@ -0,0 +1,12 @@
+<?php 
+
+echo "<pre>PHP operator 
+1. Arithmetic Operator
+2. Assigment Operator
+3. Comparison Operator
+4. Increment/Decrement Operator
+5. Logical Operator
+6. String Operator
+7. Array Operator</pre>"
+
+?>
