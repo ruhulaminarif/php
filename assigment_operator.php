dc63666cd1c179387d8c7663660fc01e8bd7d228
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-11 01:26:41 +0600

    PHP Assignment Operator

---
+++ assigment_operator.php
@@ -0,0 +1,17 @@
+<?php
+$x=20;
+$y=10;
+
+echo $x+=$y; //$x=$x+$y;
+echo "<br>";
+echo $x-=$y; //$x=$x-+$y;
+echo "<br>";
+echo $x*=$y; //$x=$x*$y;
+echo "<br>";
+echo $x/=$y; //$x=$x/$y;
+echo "<br>";
+echo $x%=$y; //$x=$x%$y;
+echo "<br>";
+echo $x**=$y; //$x=$x**$y;
+
+?>
