37fda1738e79f6108f24f2f663f7a12a75e69bfc
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-11 01:15:23 +0600

    PHP Arithmetic Operator

---
+++ arithmetic_operator.php
@@ -0,0 +1,28 @@
+<!-- + Addotion
+- Subtraction
+* Multiplication
+** Exponentiation
+/ Division
+% Modulus
+++ increment 
+-- Decrement -->
+
+<?php
+$x=20;
+$y=10;
+
+$add=$x+$y;
+$sub=$x-$y;
+$mul=$x*$y;
+$di=$x/$y;
+$ex=$x**$y;
+$mo=$x%$y;
+
+echo "Addotion: " .$add."<br>";
+echo "Subtraction: " .$sub."<br>";
+echo "Multiplication: " .$mul."<br>";
+echo "Division: " .$di."<br>";
+echo "Exponentiation: " .$ex."<br>";
+echo "Modulus: ".$mo."<br>";
+
+?>
