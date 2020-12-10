f2c84addde5b605082e33eccbadc0a2eb47c2c5f
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-11 01:38:09 +0600

    PHP Comparison Operator

---
+++ comparison_operator.php
@@ -0,0 +1,17 @@
+<!-- ==	Equal
+===	Identical(value & Data Type)		
+!=	Not equal		
+<>	Not equal		
+!==	Not identical
+>	Greater than	$x > $y	Returns true if $x is greater than $y	
+<	Less than	$x < $y	Returns true if $x is less than $y	
+>=	Greater than or equal 	
+<=	Less than or equal 
+<=>	Spaceship	$x <=> $y -->
+
+<?php
+$x=20;
+$y=20;
+
+var_dump($x==$y);
+?>
