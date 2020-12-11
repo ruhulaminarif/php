68d674798f0a63d716a130af03918906117db15a
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-11 10:33:13 +0600

    PHP Array Operator

---
+++ array_operator.php
@@ -0,0 +1,58 @@
+<?php
+$a=array(
+	'a' =>'Arif', 
+	'b' => 'Rasel'
+    );
+
+$b=array(
+	'c' =>'Ruhul',
+    'b' => 'amin'
+    );
+
+var_dump($a+$b);
+ ?>
+
+ <!-- Section -->
+
+ <?php echo "<br>"; ?>
+
+ <!-- Section -->
+
+<?php
+
+$a=array(
+	'a' =>'Arif', 
+	'b' => 'Rasel'
+    );
+
+$b=array(
+	'c' =>'Ruhul',
+    'b' => 'amin'
+    );
+
+var_dump($a===$b);
+//var_dump($a==$b);
+ ?>
+
+ <!-- Section -->
+
+  <?php echo "<br>"; ?>
+
+  <!-- Section -->
+  
+<?php
+
+$a=array(
+	'a' =>'Arif', 
+	'b' => 'Rasel'
+    );
+
+$b=array(
+	'c' =>'Ruhul',
+    'b' => 'amin'
+    );
+
+var_dump($a!=$b);
+//var_dump($a!==$b);
+//var_dump($a<>$b);
+ ?>
