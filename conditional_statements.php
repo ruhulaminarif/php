a87f85825d607ded33b23ae0a82bd22a64b3b6bd
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-11 11:33:55 +0600

    If,elseif,else Statements

---
+++ conditional_statements.php
@@ -0,0 +1,42 @@
+<?php 
+// if (Use when One Statement true)
+// if else (Use when One Statement true & One Statement False)
+// elseif (Use Multiple Statement)
+
+ ?>
+
+<!-- Section -->
+
+ <?php 
+$x=10;
+if ($x>5) {
+	echo "True <br>";
+}
+ ?>
+
+ <!-- Section -->
+
+ <?php 
+$X=10;
+
+if ($x>15) {
+	echo "True <br>";
+}else{
+	echo "Flase<br>";
+}
+
+  ?>
+
+  <!-- Section -->
+
+  <?php 
+$x=10;
+if ($x>15) {
+	echo "X is greater then 15";
+}elseif ($x>12) {
+	echo "X is greater then 12";
+}else{
+	echo "please give a value smller then 10 ";
+}
+
+   ?>
