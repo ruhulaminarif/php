295cb3c0fda6efc1da42f1aa35499c3e39679d52
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-11 11:48:36 +0600

    Switch

---
+++ switch.php
@@ -0,0 +1,42 @@
+<?php 
+$coding="php";
+
+switch($coding){
+case "html":
+echo "I Love HTML";
+break;
+
+case "mhp":
+echo "I Love PHP";
+break;
+
+default:
+echo "Coding Not Found";
+
+}
+
+ ?>
+
+ <?php
+echo "<br>";
+?>
+
+ <?php 
+$x=10;
+
+switch($x){
+
+	case 5:
+	echo "input 5";
+	break;
+
+	case 15:
+	echo "Input 15";
+	break;
+
+	default:
+	echo "Input not match";
+	
+}
+
+  ?>
