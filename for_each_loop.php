4d6b6099f92fddd280ca6c214abb8b25b2f461f0
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-11 22:59:30 +0600

    PHP Foreach Loop

---
+++ for_each_loop.php
@@ -0,0 +1,23 @@
+<?php 
+$color=array("red","blue","green","black" );
+
+foreach($color as $colors){
+     echo "$colors <br>";
+}
+
+ ?>
+
+<!--  Section -->
+
+
+ <?php echo "<br>"; ?>
+
+ <!--  Section -->
+
+ <?php 
+$site=array("google.com","facebook.com","yahoo.com");
+foreach($site as $x){
+  echo "<a href='http://$x' target='_blank'>$x </a><br>";
+}
+
+  ?>
